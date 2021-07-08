926d47ac8696e8147eea60fac123c6725a3d5974
Simon Rausch-Schott <simon.rauschschott@gmail.com>
2021-07-08 16:21:08 +0200

    v1.0.0 final implementation of console collector

--- FuelWizardConsoleCollector/Program.cs
+++ FuelWizardConsoleCollector/Program.cs
@@ -8,18 +8,81 @@ namespace FuelWizardConsoleCollector
     {
         static void Main(string[] args)
         {
+            if(args.Length < 1)
+            {
+                Console.WriteLine("You must specify the collection delay. Usage: \"current.exe <delay in following format (hh:mm:ss)>\"\nExample: " +
+                    "\n\"FuelWizardConsoleCollector.exe 01:00:00\" for 1 hour delay");
+                return;
+            }
+
+            TimeSpan delay;
+
+            try
+            {
+                delay = TimeSpan.Parse(args[0]);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Delay is not parsable. Specify the delay in this format: hh:mm:ss!");
+                return;
+            }
+            bool startRightAway = true;
+
+            if(args.Length == 2)
+            {
+                try
+                {
+                    startRightAway = bool.Parse(args[1]);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Second parameter is not parsable. Specify whether or not the DataCollector should start right away using \"True\" or \"False\"!\n" +
+                        "You can also leave it blank, which means that collecting will start directly.");
+                    return;
+                }
+                
+            }
+
+            //Print current status
+            Console.WriteLine("FuelWizard - DataCollector");
+            Console.WriteLine(" - ByIconic 2021");
+            Console.WriteLine("Delay: " + delay);
+            Console.WriteLine("Starting: " + (startRightAway ? "Now" : "After delay"));
+
+            Console.WriteLine();
+
             FuelWizardDataCollector collector = new FuelWizardDataCollector();
             collector.OnDataCollected += Collector_OnDataCollected;
-            collector.StartCollectingData(new TimeSpan(0, 0, 10));
+            collector.OnDataCollectionStarted += Collector_OnDataCollectionStarted;
+            collector.OnDataCollectionStopped += Collector_OnDataCollectionStopped;
+            collector.StartCollectingData(delay, startRightAway);
 
             Console.ReadKey();
+
+            collector.OnDataCollected -= Collector_OnDataCollected;
+            collector.StopCollectingData();
+        }
+
+        private static void Collector_OnDataCollectionStopped(object sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Stopped collecting data.");
+            Console.ResetColor();
+        }
+
+        private static void Collector_OnDataCollectionStarted(object sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Started collecting data.");
+            Console.ResetColor();
         }
 
         private static void Collector_OnDataCollected(object sender, int gasStationId, string fuelType, double price, DateTime time)
         {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.WriteLine($"[{time}] {gasStationId}: {fuelType} {price}");
-
-            
+            Console.ResetColor();
         }
     }
 }
